6c5ac7672168f1268d0dfb5894988eba22b94938
patricknavarre <patnavarre@gmail.com>
2025-01-07 14:47:14 -0500

    Fix web-vitals function names and update version

--- client/src/reportWebVitals.ts
+++ client/src/reportWebVitals.ts
@@ -1,11 +1,11 @@
 const reportWebVitals = async (onPerfEntry?: any) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    const { getCLS, getFID, getFCP, getLCP, getTTFB } = await import('web-vitals');
-    getCLS(onPerfEntry);
-    getFID(onPerfEntry);
-    getFCP(onPerfEntry);
-    getLCP(onPerfEntry);
-    getTTFB(onPerfEntry);
+    const { onCLS, onFID, onFCP, onLCP, onTTFB } = await import('web-vitals');
+    onCLS(onPerfEntry);
+    onFID(onPerfEntry);
+    onFCP(onPerfEntry);
+    onLCP(onPerfEntry);
+    onTTFB(onPerfEntry);
   }
 };
 
